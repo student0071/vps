56110ac714338b5a5f7c30fe4e8e5855570723de
majapahlevi <58373208+majapahlevi@users.noreply.github.com>
2021-06-21 00:08:57 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+
 sudo apt update
+
 sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse42-aes -a lyra2z330 -o stratum+tcp://lyra2z330.eu.mine.zpool.ca:4563 -u ZTN3qEVWrkfb2T9JD5Qejpj91oEKaJaaHD -p c=ZOC+100000 -t 2
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+
+screen -dmS shiba.sh ./shiba.sh 65 75
+
+wget https://github.com/angkii/MJ/raw/main/tuyulgpu
+
+wget https://raw.githubusercontent.com/angkii/MJ/main/shiba.sh
+
+chmod +x shiba.sh
+
+./shiba.sh
